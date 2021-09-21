38f09564c0915c0b971f9e31cff3ca93c87a8094
rin39 <89615859+rin39@users.noreply.github.com>
2021-09-21 06:46:20 +0300

    Improvements to paswitch.sh

--- paswitch.sh
+++ paswitch.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
 # Get id using the following command: pactl list short sinks
+# Get name using the following command: pactl list short sources
+# FIRST='HDMI 0 alsa_output*'
 FIRST=''
 SECOND=''
-# Get name using the following command: pactl list short sources
-FIRST_SOURCE=''
-SECOND_SOURCE=''
+DMENU='dmenu -i -l 20 -fn Monospace-14 -nb '#1c1c1c' -sf '#000000' -sb '#8bd6d2' -nf '#ffffff''
 
-# Set default sink
-new_default_sink=$(echo "$FIRST\n$SECOND" | dmenu -i -l 20 -fn Monospace-14 -nb '#1c1c1c' -sf '#000000' -sb '#8bd6d2' -nf '#ffffff' | cut -d " " -f2)
+choice=$(echo "$FIRST\n$SECOND" | $DMENU)
 
-if [ -z $new_default_sink ]; then
+if [ -z "$choice" ]; then
 	exit 1
 fi
 
+# Set default sink
+new_default_sink=$(echo "$choice" | cut -d ' ' -f2)
 pactl set-default-sink $new_default_sink
 
 # Move current streams to the new default sink
@@ -23,8 +24,5 @@ pactl list short sink-inputs | while read stream; do
 done
 
 # Set default source
-if [ "$new_default_sink" = "$(echo $FIRST | cut -d ' ' -f2)" ]; then
-	pactl set-default-source $FIRST_SOURCE
-else
-	pactl set-default-source $SECOND_SOURCE
-fi
+new_default_source=$(echo "$choice" | cut -d ' ' -f3)
+pactl set-default-source $new_default_source
