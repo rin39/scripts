02ea6864b0b6e0f36bf54d5a5e45e932994758b0
rin39 <89615859+rin39@users.noreply.github.com>
2021-09-28 17:16:55 +0300

    Improvements to ytclip

--- ytclip.sh
+++ ytclip.sh
@@ -5,15 +5,15 @@
 DIRECTORY="$HOME/example_directory"
 
 url=$(xclip -o -selection clipboard)
-echo $url
+echo "$url"
 
 echo "Enter start time (00:01:00)"
-read start_time
+read -r start_time
 echo "Enter end time (00:02:00)"
-read end_time
+read -r end_time
 
 duration="$(($(date -d "$start_time" '+%s') - $(date -d "$end_time" '+%s')))"
 duration=$( echo $duration | cut -c 2- )
 
-ffmpeg -ss "$start_time" -i $(youtube-dl -f best --get-url "$url") \
+ffmpeg -ss "$start_time" -i "$(youtube-dl -f best --get-url "$url")" \
 -t "$duration" -c:v copy -c:a copy "$DIRECTORY/$(date +%s).mkv"
